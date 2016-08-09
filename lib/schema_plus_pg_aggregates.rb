cfc36d3b674cfba5cd8563c61918876fb48f4b0f
mvgijssel <maarten@hackerone.com>
2016-08-09 11:37:27 +0200

    Require schem_plus/core

--- lib/schema_plus_pg_aggregates.rb
+++ lib/schema_plus_pg_aggregates.rb
@@ -1,3 +1,4 @@
+require 'schema_plus/core'
 require_relative 'schema_plus_pg_aggregates/aggregate_definition'
 require_relative 'schema_plus_pg_aggregates/active_record/connection_adapters/postgresql_adapter'
 require_relative 'schema_plus_pg_aggregates/middleware/postgresql/dumper'
