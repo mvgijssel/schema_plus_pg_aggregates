c2e95d9b95d438e1e568ca10a80c1972c4369b2e
mvgijssel <maarten@hackerone.com>
2016-08-09 11:56:34 +0200

    Updated version location

--- lib/schema_plus_pg_aggregates.rb
+++ lib/schema_plus_pg_aggregates.rb
@@ -1,7 +1,7 @@
 require 'schema_plus/core'
+require_relative 'schema_plus_pg_aggregates/version'
 require_relative 'schema_plus_pg_aggregates/aggregate_definition'
 require_relative 'schema_plus_pg_aggregates/active_record/connection_adapters/postgresql_adapter'
 require_relative 'schema_plus_pg_aggregates/middleware/postgresql/dumper'
-require_relative 'schema_plus_pg_aggregates/version'
 
 SchemaMonkey.register SchemaPlusPgAggregates
