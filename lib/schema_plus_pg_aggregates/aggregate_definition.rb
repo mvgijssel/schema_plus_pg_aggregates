e175ca88099b860ead94e4619218b99e6b65a90e
mvgijssel <maarten@hackerone.com>
2016-08-09 11:37:46 +0200

    Don't use module shorthand

--- lib/schema_plus_pg_aggregates/aggregate_definition.rb
+++ lib/schema_plus_pg_aggregates/aggregate_definition.rb
@@ -1,11 +1,13 @@
-class SchemaPlusPgAggregates::AggregateDefinition
-  attr_reader :name, :arguments, :state_function, :state_data_type, :initial_condition
+module SchemaPlusPgAggregates
+  class AggregateDefinition
+    attr_reader :name, :arguments, :state_function, :state_data_type, :initial_condition
 
-  def initialize(name:, arguments:, state_function:, state_data_type:, initial_condition:)
-    @name = name
-    @arguments = arguments
-    @state_function = state_function
-    @state_data_type = state_data_type
-    @initial_condition = initial_condition
+    def initialize(name:, arguments:, state_function:, state_data_type:, initial_condition:)
+      @name = name
+      @arguments = arguments
+      @state_function = state_function
+      @state_data_type = state_data_type
+      @initial_condition = initial_condition
+    end
   end
 end
