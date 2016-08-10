8dc3ed479cf497976e91592268956aac1cb0ef8c
mvgijssel <maarten@hackerone.com>
2016-08-10 17:28:40 +0200

    Created function to drop all aggregates and functions and call after each spec

--- spec/schema_plus_pg_aggregates_spec.rb
+++ spec/schema_plus_pg_aggregates_spec.rb
@@ -6,35 +6,21 @@ describe 'pg_aggregates', postgresql: :only do
 
   describe 'aggregates' do
     it 'returns all aggregates' do
-      begin
-        create_aggregate('someagg1')
-        create_aggregate('someagg2')
-
-        expect(migration.aggregates.count).to eq 2
-      ensure
-        drop_aggregate('someagg1')
-        drop_aggregate('someagg2')
-      end
+      create_aggregate('someagg1')
+      create_aggregate('someagg2')
+      expect(migration.aggregates.count).to eq 2
     end
   end
 
   describe 'create_aggregate' do
     it 'creates the aggregate' do
-      begin
-        expect { create_aggregate }.to change { migration.aggregates.count }.from(0).to(1)
-      ensure
-        drop_aggregate
-      end
+      expect { create_aggregate }.to change { migration.aggregates.count }.from(0).to(1)
     end
 
     it 'creates an aggregate when no arguments are given' do
-      begin
-        create_aggregate "someagg", arguments: []
-        expect(migration.aggregates.count).to eq 1
-        expect(migration.aggregates[0].arguments).to eq []
-      ensure
-        drop_aggregate "someagg", arguments: []
-      end
+      create_aggregate "someagg", arguments: []
+      expect(migration.aggregates.count).to eq 1
+      expect(migration.aggregates[0].arguments).to eq []
     end
   end
 
--- spec/support/object_creation_methods.rb
+++ spec/support/object_creation_methods.rb
@@ -7,31 +7,7 @@ module SchemaPlusPgAggregates::ObjectCreationMethods
       arguments: arguments
   end
 
-  # TODO: need to have some sort of After rspec hook which cleans out all created aggregates
-  # after each spec
-  def drop_aggregate(name = 'someagg', arguments: ['integer'])
-    ActiveRecord::Base.connection.drop_aggregate name, arguments: arguments
-    drop_function("#{name}func", arguments: arguments + ['integer'])
-  end
 
-  def create_function(name = 'someaggfunc', arguments: ['integer', 'integer'])
-    function_arguments = arguments.each_with_index.map do |argument, index|
-      "$#{index + 1}"
-    end
-    ActiveRecord::Base.connection.execute <<-SQL
-      CREATE FUNCTION #{name}(#{arguments.join(', ')}) RETURNS integer
-        LANGUAGE sql
-        AS $_$
-          SELECT #{function_arguments.join(' + ')}
-        $_$;
-    SQL
-  end
-
-  def drop_function(name = 'somefunc', arguments: ['integer', 'integer'])
-    ActiveRecord::Base.connection.execute <<-SQL
-      DROP FUNCTION #{name}(#{arguments.join(', ')})
-    SQL
-  end
 
   def drop_aggregates
     results = ActiveRecord::Base.connection.query(<<-SQL, 'SCHEMA')
@@ -54,9 +30,44 @@ module SchemaPlusPgAggregates::ObjectCreationMethods
     end
   end
 
+  # TODO: these two helpers should be in the pg_functions gem
+  def create_function(name = 'someaggfunc', arguments: ['integer', 'integer'])
+    function_arguments = arguments.each_with_index.map do |argument, index|
+      "$#{index + 1}"
+    end
+    ActiveRecord::Base.connection.execute <<-SQL
+      CREATE FUNCTION #{name}(#{arguments.join(', ')}) RETURNS integer
+        LANGUAGE sql
+        AS $_$
+          SELECT #{function_arguments.join(' + ')}
+        $_$;
+    SQL
+  end
+
+  def drop_functions
+    results = ActiveRecord::Base.connection.query(<<-SQL, 'SCHEMA')
+      SELECT
+        'DROP FUNCTION ' || n.nspname || '.' || proname || '(' || oidvectortypes(proargtypes) || ')' || ' CASCADE;'
+      FROM
+        pg_proc p
+      INNER JOIN pg_namespace n ON n.oid = p.pronamespace
+      INNER JOIN pg_language l ON l.oid = p.prolang
+      WHERE p.proisagg = false
+        AND n.nspname = ANY (current_schemas(false))
+        AND l.lanname NOT IN ('c', 'internal')
+    SQL
+
+    results.each do |result|
+      # TODO for some reason every result is an array with a single item?
+      ActiveRecord::Base.connection.execute result[0]
+    end
+  end
+
   RSpec.configure do |config|
+    config.include SchemaPlusPgAggregates::ObjectCreationMethods
     config.after :each do |example|
       drop_aggregates
+      drop_functions
     end
   end
 end
